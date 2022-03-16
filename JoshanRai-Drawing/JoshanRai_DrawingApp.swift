b0387a553c16ebe5d70dc81015da6b4f761c7cd7
Joshan Rai <pradheon@gmail.com>
2022-03-15 22:51:27 -0700

    Initial Commit

---
+++ JoshanRai-Drawing/JoshanRai_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoshanRai_DrawingApp.swift
+//  JoshanRai-Drawing
+//
+//  Created by Joshan Rai on 3/15/22.
+//
+
+import SwiftUI
+
+@main
+struct JoshanRai_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
