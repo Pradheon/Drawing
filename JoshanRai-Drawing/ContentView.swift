18580ebb0b9ab7495df456712b7e14018bb2232b
Joshan Rai <pradheon@gmail.com>
2022-03-16 15:34:01 -0700

    v0.3 Update
    - Following Day 43 of 100 Days of SwiftUI curriculum. https://www.hackingwithswift.com/100/swiftui/43
    - Creating custom paths with SwiftUI
    - Paths vs shapes in SwiftUI
    - Adding strokeBorder() support with InsettableShape

--- JoshanRai-Drawing/ContentView.swift
+++ JoshanRai-Drawing/ContentView.swift
@@ -7,10 +7,86 @@
 
 import SwiftUI
 
+//   Paths vs shapes in SwiftUI
+/*
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}
+ */
+
+ //   Paths vs shapes in SwiftUI &  Adding strokeBorder() support with InsettableShape
+struct Arc: InsettableShape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+    var insetAmount = 0.0
+    
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+        
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2 - insetAmount, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+        
+        return path
+    }
+    
+    func inset(by amount: CGFloat) -> some InsettableShape {
+        var arc = self
+        arc.insetAmount += amount
+        return arc
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        //   Creating custom paths with SwiftUI
+        /*
+        Path { path in
+            path.move(to: CGPoint(x: 200, y: 100))
+            path.addLine(to: CGPoint(x: 100, y: 300))
+            path.addLine(to: CGPoint(x: 300, y: 300))
+            path.addLine(to: CGPoint(x: 200, y: 100))
+            //path.closeSubpath()
+        }
+        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+        //.stroke(.blue, lineWidth: 10)
+        //.fill(.blue)
+         */
+        
+        //   Paths vs shapes in SwiftUI
+        /*
+        Triangle()
+            .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+            //.fill(.red)
+            .frame(width: 300, height: 300)
+        
+        Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
+            .stroke(.blue, lineWidth: 10)
+            .frame(width: 300, height: 300)
+         */
+        
+        //   Adding strokeBorder() support with InsettableShape
+        /*
+        Circle()
+            .stroke(.blue, lineWidth: 40)
+         */
+        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
+            .strokeBorder(.blue, lineWidth: 40)
+        
+        //   Placeholder so the code doesn't complain during comments
+        //Text("Hello, Sekai!")
     }
 }
 
