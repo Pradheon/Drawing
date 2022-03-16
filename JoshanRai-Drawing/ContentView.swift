66aa5ac3889ce07e39d30e992ef0ef9300461f3d
Joshan Rai <pradheon@gmail.com>
2022-03-16 15:56:50 -0700

    v0.4 Update
    - Following Day 44 of "100 Days of SwiftUI" by hackingwithswift.com
    - Transforming shapes using CGAffineTransform and even-odd fills

--- JoshanRai-Drawing/ContentView.swift
+++ JoshanRai-Drawing/ContentView.swift
@@ -24,6 +24,7 @@ struct Triangle: Shape {
  */
 
  //   Paths vs shapes in SwiftUI &  Adding strokeBorder() support with InsettableShape
+/*
 struct Arc: InsettableShape {
     var startAngle: Angle
     var endAngle: Angle
@@ -47,8 +48,48 @@ struct Arc: InsettableShape {
         return arc
     }
 }
+ */
+
+//   Transforming shapes using CGAffineTransform and even-odd fills
+struct Flower: Shape {
+    // How much to move this petal away from the center
+    var petalOffset: Double = -20
+    
+    // How wide to make each petal
+    var petalWidth: Double = 100
+    
+    func path(in rect: CGRect) -> Path {
+        // The path that will hold all petals
+        var path = Path()
+        
+        // Count from 0 up to pi * 2, moving up pi / 8 each time
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8) {
+            // rotate the petal by the current value of our loop
+            let rotation = CGAffineTransform(rotationAngle: number)
+            
+            // move the petal to be at the cener of our view
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            // create a path for this petal using our properties plus a fixed Y and height
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            
+            // apply our rotation/position tranformation to the petal
+            let rotatedPetal = originalPetal.applying(position)
+            
+            // add it to our main path
+            path.addPath(rotatedPetal)
+        }
+        
+        // now send the main path back
+        return path
+    }
+}
 
 struct ContentView: View {
+    //   Transforming shapes using CGAffineTransform and even-odd fills
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    
     var body: some View {
         
         //   Creating custom paths with SwiftUI
@@ -82,8 +123,25 @@ struct ContentView: View {
         Circle()
             .stroke(.blue, lineWidth: 40)
          */
+        /*
         Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
             .strokeBorder(.blue, lineWidth: 40)
+         */
+        
+        //   Transforming shapes using CGAffineTransform and even-odd fills
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                //.stroke(.red, lineWidth:  1)
+                .fill(.red, style: FillStyle(eoFill: true))
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
         
         //   Placeholder so the code doesn't complain during comments
         //Text("Hello, Sekai!")
