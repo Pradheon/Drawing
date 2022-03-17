d8459647c476ea4740d6b309a5497a90879f49f8
Joshan Rai <pradheon@gmail.com>
2022-03-17 10:14:27 -0700

    v0.9 Update
    - Day 45 of "100 Days of SwiftUI" from hackingwithswift.com
    - Animating complex shapes with AnimatablePair

--- JoshanRai-Drawing/ContentView.swift
+++ JoshanRai-Drawing/ContentView.swift
@@ -128,6 +128,7 @@ struct ColorCyclingCircle: View {
  */
 
 //   Animating simple shapes with animatableData
+/*
 struct Trapezoid: Shape {
     var insetAmount: Double
     
@@ -148,6 +149,45 @@ struct Trapezoid: Shape {
         return path
     }
 }
+ */
+
+//   Animating complex shapes with AnimatablePair
+struct Checkerboard: Shape {
+    var rows: Int
+    var columns: Int
+    
+    var animatableData: AnimatablePair<Double, Double> {
+        get {
+            AnimatablePair(Double(rows), Double(columns))
+        }
+        
+        set {
+            rows = Int(newValue.first)
+            columns = Int(newValue.second)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        let rowSize = rect.height / Double(rows)
+        let columnSize = rect.width / Double(columns)
+        
+        for row in 0..<rows {
+            for column in 0..<columns {
+                if (row + column).isMultiple(of: 2) {
+                    let startX = columnSize * Double(column)
+                    let startY = rowSize * Double(row)
+                    
+                    let rect = CGRect(x: startX, y: startY, width: columnSize, height: rowSize)
+                    path.addRect(rect)
+                }
+            }
+        }
+        
+        return path
+    }
+}
 
 struct ContentView: View {
     //   Transforming shapes using CGAffineTransform and even-odd fills
@@ -163,7 +203,11 @@ struct ContentView: View {
     //@State private var amount = 0.0
     
     //   Animating simple shapes with animatableData
-    @State private var insetAmount = 50.0
+    //@State private var insetAmount = 50.0
+    
+    //   Animating complex shapes with AnimatablePair
+    @State private var rows = 4
+    @State private var columns = 4
     
     var body: some View {
         
@@ -299,11 +343,22 @@ struct ContentView: View {
         */
         
         //   Animating simple shapes with animatableData
+        /*
         Trapezoid(insetAmount: insetAmount)
             .frame(width: 200, height: 100)
             .onTapGesture {
                 withAnimation {
                     insetAmount = Double.random(in: 10...90)
+                }
+            }
+         */
+        
+        //   Animating complex shapes with AnimatablePair
+        Checkerboard(rows: rows, columns: columns)
+            .onTapGesture {
+                withAnimation(.linear(duration: 3)) {
+                    rows = 8
+                    columns = 16
                 }
             }
         
