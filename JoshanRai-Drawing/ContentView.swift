a859c3683c6d14d0874743c27ec32385eca27b45
Joshan Rai <pradheon@gmail.com>
2022-03-17 10:33:33 -0700

    v1.0 Update
    - Day 45 of "100 Days of SwiftUI" from hackingwithswift.com
    - Creating a spirograph with SwiftUI

--- JoshanRai-Drawing/ContentView.swift
+++ JoshanRai-Drawing/ContentView.swift
@@ -152,6 +152,7 @@ struct Trapezoid: Shape {
  */
 
 //   Animating complex shapes with AnimatablePair
+/*
 struct Checkerboard: Shape {
     var rows: Int
     var columns: Int
@@ -188,6 +189,56 @@ struct Checkerboard: Shape {
         return path
     }
 }
+ */
+
+//   Creating a spirograph with SwiftUI
+struct Spirograph: Shape {
+    let innerRadius: Int
+    let outerRadius: Int
+    let distance: Int
+    let amount: Double
+    
+    func gcd(_ a: Int, _ b: Int) -> Int {
+        var a = a
+        var b = b
+        
+        while b != 0 {
+            let temp = b
+            b = a % b
+            a = temp
+        }
+        
+        return a
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        let divisor = gcd(innerRadius, outerRadius)
+        
+        let outerRadius = Double(self.outerRadius)
+        let innerRadius = Double(self.innerRadius)
+        let distance = Double(self.distance)
+        let difference = innerRadius - outerRadius
+        let endPoint = ceil(2 * Double.pi * outerRadius / Double(divisor)) * amount
+        
+        var path = Path()
+        
+        for theta in stride(from: 0, through: endPoint, by: 0.01) {
+            var x = difference * cos(theta) + distance * cos(difference / outerRadius * theta)
+            var y = difference * sin(theta) - distance * sin(difference / outerRadius * theta)
+            
+            x += rect.width / 2
+            y += rect.height / 2
+            
+            if theta == 0 {
+                path.move(to: CGPoint(x: x, y: y))
+            } else {
+                path.addLine(to: CGPoint(x: x, y: y))
+            }
+        }
+        
+        return path
+    }
+}
 
 struct ContentView: View {
     //   Transforming shapes using CGAffineTransform and even-odd fills
@@ -206,8 +257,17 @@ struct ContentView: View {
     //@State private var insetAmount = 50.0
     
     //   Animating complex shapes with AnimatablePair
+    /*
     @State private var rows = 4
     @State private var columns = 4
+     */
+    
+    //   Creating a spirograph with SwiftUI
+    @State private var innerRadius = 125.0
+    @State private var outerRadius = 75.0
+    @State private var distance = 25.0
+    @State private var amount = 1.0
+    @State private var hue = 0.6
     
     var body: some View {
         
@@ -354,6 +414,7 @@ struct ContentView: View {
          */
         
         //   Animating complex shapes with AnimatablePair
+        /*
         Checkerboard(rows: rows, columns: columns)
             .onTapGesture {
                 withAnimation(.linear(duration: 3)) {
@@ -361,6 +422,40 @@ struct ContentView: View {
                     columns = 16
                 }
             }
+         */
+        
+        //   Creating a spirograph with SwiftUI
+        VStack(spacing: 0) {
+            Spacer()
+            
+            Spirograph(innerRadius: Int(innerRadius), outerRadius: Int(outerRadius), distance: Int(distance), amount: amount)
+                .stroke(Color(hue: hue, saturation: 1, brightness: 1), lineWidth: 1)
+                .frame(width: 300, height: 300)
+            
+            Spacer()
+            
+            Group {
+                Text("Inner Radius: \(Int(innerRadius))")
+                Slider(value: $innerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Outer Radius: \(Int(outerRadius))")
+                Slider(value: $outerRadius, in: 10...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Distance: \(Int(distance))")
+                Slider(value: $distance, in: 1...150, step: 1)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Amount: \(amount, format: .number.precision(.fractionLength(2)))")
+                Slider(value: $amount)
+                    .padding([.horizontal, .bottom])
+                
+                Text("Color")
+                Slider(value: $hue)
+                    .padding(.horizontal)
+            }
+        }
         
         //   Placeholder so the code doesn't complain during comments
         //Text("Hello, Sekai!").padding()
